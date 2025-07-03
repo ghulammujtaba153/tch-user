bb34c587399e4a8bf83b459908a2b4a0934531b4
Mujtaba12-al <mujtaba@mativeinc.com>
2025-07-03 15:54:01 +0500

    payment modified1

--- src/components/home/CollaborationSection.tsx
+++ src/components/home/CollaborationSection.tsx
@@ -1,36 +1,39 @@
-// import React from 'react';
-
-// const CollaborationSection: React.FC = () => {
-//   return (
-//     <section className="w-full py-16 text-black">
-//       <div className="flex flex-col items-center justify-center gap-10">
-//         <p className="text-sm font-bold">
-//           We Collaborate with the top 200+ companies worldwide
-//         </p>
-
-//         <div className="relative flex items-center justify-between w-[90%]">
-//           {/* Gradient Overlay */}
-//           <div className="absolute left-0 top-0 h-full w-32 bg-gradient-to-r from-white to-transparent"></div>
-//           <div className="absolute right-0 top-0 h-full w-32 bg-gradient-to-l from-white to-transparent"></div>
-
-//           {/* Company Logos */}
-//           <img src="/collaborate-1.png" alt="logo" width={200} height={200} />
-//           <img src="/collaborate-1.png" alt="logo" width={200} height={200} />
-//           <img src="/collaborate-1.png" alt="logo" width={200} height={200} />
-//           <img src="/collaborate-1.png" alt="logo" width={200} height={200} />
-//         </div>
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default CollaborationSection;
 
 
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { BASE_URL, SOCKET_URL } from '../../config/url';
+import { toast } from 'react-toastify';
 
-import React from 'react';
+
+const getFullUrl = (filePath: string) =>
+  filePath?.startsWith('http') ? filePath : `${SOCKET_URL}/${filePath}`;
 
 const CollaborationSection: React.FC = () => {
+
+
+  const [data, setData] = useState()
+  const [loading, setLoading] = useState(true)
+
+
+  const fetch = async () => {
+    try {
+      const res = await axios.get(`${BASE_URL}/organization`); 
+      setData(res.data);
+    } catch (error) {
+      console.log(error);
+      toast.error('Error fetching data');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+
+  useEffect(() => {
+    fetch()
+  }, [])
+
+
   return (
     <section className="max-w-[1200px] mx-auto py-6 font-onest text-black">
       <div className="flex flex-col items-center justify-center gap-10">
@@ -44,10 +47,8 @@ const CollaborationSection: React.FC = () => {
           <div className="absolute right-0 top-0 h-full w-16 sm:w-24 bg-gradient-to-l from-bg to-transparent"></div>
 
           {/* Company Logos */}
-          <img src="/collaborate-1.png" alt="logo" className="w-24 sm:w-32 md:w-40 lg:w-48" />
-          <img src="/collaborate-1.png" alt="logo" className="w-24 sm:w-32 md:w-40 lg:w-48" />
-          <img src="/collaborate-1.png" alt="logo" className="w-24 sm:w-32 md:w-40 lg:w-48" />
-          <img src="/collaborate-1.png" alt="logo" className="w-24 sm:w-32 md:w-40 lg:w-48" />
+          {data && data.map((item: any) => <img src={getFullUrl(item.logo)} alt="/" className="w-24 sm:w-32 md:w-40 lg:w-48" />) || <img src="/collaborate-1.png" alt="logo" className="w-24 sm:w-32 md:w-40 lg:w-48" />}
+          
           
         </div>
       </div>
